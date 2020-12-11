e440bb8892131a36edec240a8ee2f5152e0ad209
anthonyfinix <anthonyfinix@gmail.com>
2020-12-11 12:10:06 +0530

    profile ui updated

--- ultimate-member/templates/profile.php
+++ ultimate-member/templates/profile.php
@@ -29,7 +29,7 @@ if (isset($_POST['submit'])) {
 		update_user_meta($user->ID, 'servicesOffered', $serviceOffered);
 		echo '<div class="alert alert-success" role="alert">Services and Description updated</div>';
 	}
-	if (isset($address) && !empty($address)){
+	if (isset($address) && !empty($address)) {
 		update_user_meta($user->ID, 'address', $address);
 		echo '<div class="alert alert-success" role="alert">Address Updated</div>';
 	}
@@ -46,19 +46,19 @@ if (isset($_POST['submit'])) {
 		}
 		file_put_contents($file, $image_data);
 		$wp_filetype = wp_check_filetype($filename, null);
-		
+
 		$attachment = array(
 			'post_mime_type' => $wp_filetype['type'],
 			'post_title' => sanitize_file_name($filename),
 			'post_content' => '',
 			'post_status' => 'inherit'
 		);
-		
+
 		$attach_id = wp_insert_attachment($attachment, $file);
 		require_once(ABSPATH . 'wp-admin/includes/image.php');
 		$attach_data = wp_generate_attachment_metadata($attach_id, $file);
 		wp_update_attachment_metadata($attach_id, $attach_data);
-		
+
 		update_user_meta(get_current_user_id(), 'profile_photo', $avatar['name']);
 		move_uploaded_file($avatar['tmp_name'], _wp_upload_dir()['basedir'] . '/ultimatemember/' . get_current_user_id() . '/' . $_FILES['avatar']['name']);
 		echo '<div class="alert alert-success" role="alert">Avatar Updated</div>';
@@ -74,46 +74,59 @@ if (gettype($userMeta['profile_photo'][0]) == 'string') {
 }
 ?>
 <div class="container" style="min-height: 80vh;">
-	<div class="card card-body flex-row my-4 align-items-center justify-content-between">
-		<div>
-			<h5> <span class="font-weight-bold"> Name</span> : <?php echo $user->display_name ?></h5>
-			<h5> <span class="font-weight-bold"> Email</span> : <?php echo $user->user_email ?></h5>
-
-		</div>
+	<div class="d-flex flex-row mb-4 align-items-end" style="margin-top:-20px">
 		<div class="mr-2 rounded-circle" style="
-                width: 100px;
-                height: 100px;
-                background-image: url('<?php echo $imageUrl ?>');
-                background-size:contain;
-                background-repeat:no-repeat;
-                background-position:center;
-                ">
+				width: 100px;
+				height: 100px;
+				background-image: url('<?php echo $imageUrl ?>');
+				background-size:contain;
+				background-repeat:no-repeat;
+				background-position:center;
+				border: 5px solid white;
+				">
+		</div>
+		<div>
+			<h1 class="font-weight-light mb-4"> <?php echo $user->display_name ?></h1>
 		</div>
 	</div>
-	<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+	<ul class="nav nav-pills mb-3 mt-5" id="pills-tab" role="tablist">
 		<li class="nav-item" role="presentation">
-			<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-description" role="tab">Description</a>
+			<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-description" role="tab"> <small> Description</small></a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-contactDetails" role="tab">Contact Details</a>
-		</li>
-		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-offers" role="tab">Offers</a>
+			<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-offers" role="tab"> <small> Offers</small></a>
 		</li>
 		<?php if (get_current_user_id() == $user->ID) : ?>
-			<li class="nav-item" role="presentation">
-				<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-update" role="tab">Update Details</a>
+			<li class="nav-item ml-auto" role="presentation">
+				<a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-update" role="tab"> <small> Update Details</small></a>
 			</li>
 		<?php endif ?>
 	</ul>
 	<div class="tab-content" id="pills-tabContent">
 		<div class="tab-pane fade show active" id="pills-description" role="tabpanel">
-			<h5> <span class="font-weight-bold"> Description</span> : <?php echo $userMeta['servicesOffered'][0] ?></h5>
-		</div>
-		<div class="tab-pane fade show" id="pills-contactDetails" role="tabpanel">
-			<h5> <span class="font-weight-bold"> Email</span> : <?php echo $user->user_email ?></h5>
-			<h5> <span class="font-weight-bold"> Address</span> : <?php echo $userMeta['address'][0] ?></h5>
-			<h5> <span class="font-weight-bold"> Mobile Number</span> : <?php echo $userMeta['mobile_number'][0] ?></h5>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="card card-body">
+						<p><?php echo $userMeta['servicesOffered'][0] ?></p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-body text-white bg-primary">
+						<div class="d-flex align-items-center mb-4">
+							<span class="material-icons mr-2">mail</span>
+							<p class="mb-0"><?php echo $user->user_email ?></p>
+						</div>
+						<div class="d-flex align-items-center mb-4">
+							<span class="material-icons mr-2">call</span>
+							<p class="mb-0"><?php echo $userMeta['mobile_number'][0] ?></p>
+						</div>
+						<div class="d-flex align-items-start">
+							<span class="material-icons mr-2">location_pin</span>
+							<p class="mb-0"><?php echo $userMeta['address'][0] ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="tab-pane fade" id="pills-offers" role="tabpanel">
 			<?php
@@ -133,8 +146,8 @@ if (gettype($userMeta['profile_photo'][0]) == 'string') {
 				<form method="post" enctype="multipart/form-data">
 					<input type="text" class="form-control my-2" placeholder="Email" name="user_email">
 					<input type="text" class="form-control my-2" placeholder="Mobile Number" name="mobile_number">
-					<input type="text" class="form-control my-2" placeholder="Service Offered" name="serviceOffered">
-					<input type="text" class="form-control my-2" placeholder="Address" name="address">
+					<textarea class="form-control my-2" placeholder="Service Offered" name="serviceOffered"></textarea>
+					<textarea type="text" class="form-control my-2" placeholder="Address" name="address"></textarea>
 					<input type="file" class="form-control my-2" placeholder="Upload Avatar" name="avatar" accept="image/*" />
 					<button class="btn btn-success ml-auto" name="submit" type="submit">UPDATE</button>
 				</form>
